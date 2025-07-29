72d5f9e5803973d8a53af9af42f4dff132e0d1b3
RjTabiano <rj.tabiano@hoolisoftware.com>
2025-07-30 01:15:39 +0800

    Fix api url

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,6 +3,10 @@ import { API_URL } from './api';
 
 const apiBaseUrl = API_URL;
 
+// Debug logging
+console.log('API_URL:', API_URL);
+console.log('apiBaseUrl:', apiBaseUrl);
+
 const axiosInstance = axios.create({
   baseURL: apiBaseUrl,
   headers: {
