3460a6394393697b2c677c85ce798f8ebfdf253d
RjTabiano <157981963+RjTabiano@users.noreply.github.com>
2026-05-09 16:02:13 +0800

    fix: api URL

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,14 +1,8 @@
 import axios from 'axios';
 import { API_URL } from './api';
 
-const apiBaseUrl = API_URL;
-
-// Debug logging
-console.log('API_URL:', API_URL);
-console.log('apiBaseUrl:', apiBaseUrl);
-
 const axiosInstance = axios.create({
-  baseURL: apiBaseUrl,
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
